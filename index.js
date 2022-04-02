528c18d07529cd256b94b63626eb85763225ffc9
Martín Alejandro Barros <53456570+martincho32@users.noreply.github.com>
2022-04-02 06:21:22 +0000

    new product class was added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { User } from './User.js'
+import { Product } from './Product.js'
 
 const ryan = new User("Ryan", "Ray")
+const newProduct = new Product("laptop", 3000)
 
 console.log(ryan)
+console.log(newProduct)
